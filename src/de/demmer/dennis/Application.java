8637bcc44ad73981bd740f7775d56655c466e44d
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2022-11-08 10:57:40 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,7 +1,40 @@
 package de.demmer.dennis;
 
 public class Application {
-	
 
+	public static void main(String[] args) {
+		int i = posNeg1(-8);
+		int y = posNeg2(8);
+		
+		System.out.println(i);
+		System.out.println(y);
+
+	}
+
+	static int posNeg1(int zahl) {
+		int ergebnis = 0;
+
+		if (zahl > 0) {
+			ergebnis = 1;
+		} else if (zahl < 0) {
+			ergebnis = -1;
+		}
+
+		return ergebnis;
+	}
+
+	
+	static int posNeg2(int zahl) {
+		if (zahl > 0) {
+			return 1;
+		} else if (zahl < 0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+	
+	
+	
 
 }
